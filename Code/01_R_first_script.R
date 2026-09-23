50129f6652e10317c5e7540592aceda5c2841476
Samuele Marconi <samuele.marconi.2000@gmail.com>
2026-09-23 10:40:47 +0200

    Update and rename R_first_script.R to 01_R_first_script.R

---
+++ Code/01_R_first_script.R
@@ -0,0 +1,19 @@
+# Script for using R
+
+# operation
+2 + 3
+
+# an object
+samuele <- 2 + 3
+
+# another object
+gemma <- 4 + 6
+
+# different objects
+samuele + gemma
+samuele * gemma # instead of writing (2+3) * (4+6)
+samuele ^ gemma
+
+tma <- 5 * 4
+
+tma + samuele + gemma
